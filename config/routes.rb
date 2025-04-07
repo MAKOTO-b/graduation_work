5bdfcaa78641c359d7f4e2e142a98e46f7c807be
kosei seki <koseiseki@koseinoMacBook-Air.local>
2025-04-08 00:31:56 +0900

    model作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+    registrations: 'registrations',
+    sessions: 'sessions'
+  }
 
   root 'top#index'
-  
+
+  resources :home, only: [:index]
+  resources :chat_rooms, only: [:create, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
