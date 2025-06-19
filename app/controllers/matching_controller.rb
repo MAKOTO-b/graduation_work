e43017cd9f18fac5b7d4916d9ab6a9ae69347e31
kosei seki <koseiseki@koseinoMacBook-Air.local>
2025-06-20 04:23:20 +0900

    マッチング画面作成

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -4,21 +4,29 @@ class MatchingController < ApplicationController
 
   def index
     #テストしたいためここで他ユーザーのマッチングステータスを意図的に更新
-    #あとで
+    #テスト用コード(自分以外の全ユーザーのステータスを1へ更新)
+    test_user = User.where("id >= ?", 2)
+    test_user.update(matching_status: 1)
+    #テスト用コード(自分以外の全ユーザーのステータスを0へ更新)
+    #test_user = User.where("id >= ?", 2)
+    #test_user.update(matching_status: 0)
 
     #マッチングステータス更新
     #→退出時にマッチングステータスを削除
-    
+    #ログインユーザーのレコード取得
+    user = User.find(current_user.id)
+    user.update(matching_status: 1)
 
-    #マッチングステータスが空白ではない他のユーザーレコードをランダムで取得
-    
+    #マッチングステータスが空白ではない他のユーザーレコード取得
+    @match_users = User.where(matching_status: 1).where.not(id: current_user.id)
+
     #取得できない場合、
-     #自身のマッチングステータスを空白へ
-     #メッセージを表示してホームに戻る
-    
-    #取得できた場合、
-      #マッチングステータスが空白ではないかつ、
-      #自分以外のレコードを全件取得
-
+    #if @match_users.blank?
+      #自身のマッチングステータスを空白へ
+      #user.update(matching_status: 0)
+      #メッセージを表示してホームに戻る
+      #→現状ホームに戻るだけなのでメッセージを表示したい
+      #redirect_to home_index_path
+    #end
   end
 end
