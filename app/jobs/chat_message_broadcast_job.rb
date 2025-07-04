54b96f036948c43ce7430c6113dca29676a46421
kosei seki <koseiseki@koseinoMacBook-Air.local>
2025-07-04 12:56:37 +0900

    lint修正2

--- app/jobs/chat_message_broadcast_job.rb
+++ app/jobs/chat_message_broadcast_job.rb
@@ -3,14 +3,13 @@ class ChatMessageBroadcastJob < ApplicationJob
 
   def perform(chat_message)
     # receivedへデータを渡す
-    ActionCable.server.broadcast 'chat_room_channel', { chat_message: render_chat_message(chat_message) }
+    ActionCable.server.broadcast "chat_room_channel", { chat_message: render_chat_message(chat_message) }
   end
 
   private
 
     def render_chat_message(chat_message)
       # コントローラーの制約を受けずに、任意のビューテンプレートをレンダリングする
-      ApplicationController.renderer.render(partial: 'chat_messages/chat_message', locals: { chat_message: chat_message, current_user: chat_message.user })
+      ApplicationController.renderer.render(partial: "chat_messages/chat_message", locals: { chat_message: chat_message, current_user: chat_message.user })
     end
 end
-
