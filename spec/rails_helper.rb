aac4861d7d23be1517535d9cd9179faf09d23c88
kosei seki <koseiseki@koseinoMacBook-Air.local>
2025-08-13 10:16:46 +0900

    js読み込み

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require "capybara/rspec"
+require "selenium-webdriver"
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -90,4 +93,20 @@ RSpec.configure do |config|
   Capybara.server = :puma, { Silent: true }
 
   config.include FactoryBot::Syntax::Methods
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless=new")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,1400")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+RSpec.configure do |config|
+  config.before(type: :system) do
+    driven_by :headless_chrome
+  end
+end
+
 end
