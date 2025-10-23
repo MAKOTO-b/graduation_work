a4ae52f539152a7eb22f4121d41fc93313d0a249
kosei seki <koseiseki@koseinoMacBook-Air.local>
2025-10-23 10:15:30 +0900

    Fix:readme&lint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,        keys: [:name, :gender, :profile_image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :profile_image])
+    devise_parameter_sanitizer.permit(:sign_up,        keys: [ :name, :gender, :profile_image ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :email, :profile_image ])
   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,14 +3,14 @@ require 'fog/aws'
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage      = :fog
-    config.fog_provider = 'fog/aws'
+    config.fog_provider = "fog/aws"
     config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                ENV['AWS_REGION']
+      provider:              "AWS",
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:                ENV["AWS_REGION"]
     }
-    config.fog_directory = ENV['AWS_BUCKET']
+    config.fog_directory = ENV["AWS_BUCKET"]
 
     # ▼ここがポイント：ACLを送らない（public-read等を付けない）
     config.fog_public = false
