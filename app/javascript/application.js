4f41fe5c9d2709caca7b1724cda8b32351cc5934
kosei seki <koseiseki@koseinoMacBook-Air.local>
2025-10-23 17:10:00 +0900

    Add:before visit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,10 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 import "./channels"
 import "./profile_images/profile_image_upload"
+
+document.addEventListener("turbo:before-visit", () => {
+  // ページ遷移時にActionCableの接続を強制的に閉じる
+  if (window.App && window.App.cable) {
+    window.App.cable.disconnect();
+  }
+});
